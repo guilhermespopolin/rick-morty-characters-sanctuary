b6feb924c185aeb21c45bb3999be4626f917b6b4
Guilherme S Popolin <guilhermespopolin@gmail.com>
2019-07-25 01:48:08 -0300

    feat: added new functionality to Button component

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -1,18 +1,70 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-const Button = styled.button`
-  display: inline-block;
+const StyledButton = styled.button`
+  display: inline-flex;
+  align-items: center;
   padding: 0.5rem;
   color: ${({ theme }) => theme.colors.white};
-  background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary0};
   border: none;
   outline: none;
   border-radius: 3px;
+  min-width: 60px;
 
-  &:hover {
+  &:hover:enabled {
     cursor: pointer;
-    background-color: ${({ theme }) => theme.colors.primary2};
+    background-color: ${({ theme }) => theme.colors.primary1};
+  }
+
+  &:disabled {
+    opacity: 0.4;
+  }
+
+  &:disabled:hover {
+    cursor: not-allowed;
   }
 `
+
+function Button({
+  onClick,
+  children,
+  icon,
+  iconRight,
+  ...others
+}) {
+  const content = iconRight ? (
+    <Fragment>
+      {children}
+      {icon ? React.cloneElement(icon, { style: { marginLeft: '0.5rem' } }) : null}
+    </Fragment>
+  ) : (
+    <Fragment>
+      {icon ? React.cloneElement(icon, { style: { marginRight: '0.5rem' } }) : null}
+      {children}
+    </Fragment>
+  )
+
+  return (
+    <StyledButton onClick={onClick} {...others}>
+      {content}
+    </StyledButton>
+  )
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node,
+  icon: PropTypes.element,
+  iconRight: PropTypes.bool,
+}
+
+Button.defaultProps = {
+  onClick: () => {},
+  children: 'Click me!',
+  icon: null,
+  iconRight: false,
+}
 
 export default Button
