80e29a8c09469a21933e8cceff0bfd4476557473
Guilherme S Popolin <guilhermespopolin@gmail.com>
2019-08-02 00:53:11 -0300

    chore: added mock endpoint for characters

--- src/__mocks__/db.js
+++ src/__mocks__/db.js
@@ -19,4 +19,47 @@ function generateUsers(qtd = 50) {
   return { users }
 }
 
-module.exports = generateUsers
+function generateCharacters() {
+  const characters = [
+    {
+      id: 2,
+      name: 'Morty Smith',
+      status: 'Alive',
+      species: 'Human',
+      type: '',
+      gender: 'Male',
+      origin: {
+        name: 'Earth',
+        url: 'https://rickandmortyapi.com/api/location/1',
+      },
+      location: {
+        name: 'Earth',
+        url: 'https://rickandmortyapi.com/api/location/20',
+      },
+      image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+      episode: [
+        'https://rickandmortyapi.com/api/episode/1',
+        'https://rickandmortyapi.com/api/episode/2',
+      ],
+      url: 'https://rickandmortyapi.com/api/character/2',
+      created: '2017-11-04T18:50:21.651Z',
+    },
+  ]
+
+  return characters
+}
+
+function initializeMockedDB() {
+  return {
+    users: generateUsers(),
+    characters: {
+      info: {
+        prev: '',
+        next: 'https://rickandmortyapi.com/api/character/?page=2',
+      },
+      results: generateCharacters(),
+    },
+  }
+}
+
+module.exports = initializeMockedDB
